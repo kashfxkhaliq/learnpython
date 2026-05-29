8c5187aefe46240368be3d6462c7b3599b29f6b7
kashfx <kashfxkhaliq@gmail.com>
2026-05-29 09:24:37 +0500

    Arithmetic operators

---
+++ arithmetic_op.py
@@ -0,0 +1,18 @@
+
+#Arithmetic operators
+
+a = 10
+b = 20
+
+print(a + b)
+
+c = int(input("Enter the Number : "))
+d = int(input("Enter the Number : "))
+
+print("Sum of Numbers = ", c + d)
+print("subtraction of Numbers =", c - d)
+print("Multiplication of Numbers = ", c * d)
+print("Division of Numbers", c / d)
+print("Modulus of Numbers", c % d)
+print("power of Numbers", c ** d)
+print("Floor Division of Numbers", c // d)
