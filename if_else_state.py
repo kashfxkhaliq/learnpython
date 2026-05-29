67b38e57d04a42091f8a02554fc73440a1da680a
kashfx <kashfxkhaliq@gmail.com>
2026-05-29 09:43:52 +0500

    Added if else

---
+++ if_else_state.py
@@ -0,0 +1,11 @@
+
+num = int(input("Enter the Number :: "))
+
+if num % 2 == 0:
+    print("The entered number is Even")
+
+else:
+    print("The entered number is Odd")
+
+
+print ("Even Number") if num % 2 == 0 else print("Odd Number")
