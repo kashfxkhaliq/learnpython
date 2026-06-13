8c5acdc9df27d4c6c7de168bcd5d65f8bd484a7a
kashfx <kashfxkhaliq@gmail.com>
2026-06-13 15:29:35 +0500

    Assignment Operator

---
+++ assignment_op.py
@@ -0,0 +1,22 @@
+# Assignment operator
+
+a = int(input("Enter the Number : "))
+b = int(input("Enter the Number : "))
+
+a += b
+print("a After Addition = ", a)
+
+a -= b
+print("a After Subtraction = ", a)
+
+a *= b
+print("a After Multiplication = ", a)
+
+a /= b
+print("a After Division = ", a)
+
+a %= b
+print("a After Modulus = ", a)
+
+a **= b
+print("a After Exponention = ", a)
