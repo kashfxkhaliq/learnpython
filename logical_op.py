978e74a6d30a21ec685065f4afc359660cdfd169
kashfx <kashfxkhaliq@gmail.com>
2026-06-13 15:32:28 +0500

    Logical Operator

---
+++ logical_op.py
@@ -0,0 +1,15 @@
+
+x = True
+y = False
+
+#Logical AND
+logical_and = x and y
+print(logical_and)
+
+#Logical OR
+logical_or = x or y
+print(logical_or)
+
+#Logical NOT
+logical_not = not x
+print(logical_and)
