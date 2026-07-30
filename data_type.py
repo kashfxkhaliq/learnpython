37a0d6f41ac06230418a9f37aefe229388ac52ed
kashfx <kashfxkhaliq@gmail.com>
2026-07-30 19:48:48 +0500

    Rmove Files

--- data_type.py
+++
@@ -1,7 +0,0 @@
-
-
-name= str(input("Enter the Name :: "))
-height = float(input("Enter the Height :: "))
-marks = int(input("Enter the Marks :: "))
-
-print(type(name), type(height), type(marks))
--- if_else_state.py
+++
@@ -1,8 +0,0 @@
-
-num = int(input("Enter the Number :: "))
-
-if num % 2 == 0:
-    print("The entered number is Even")
-
-else:
-    print("The entered number is Odd")
